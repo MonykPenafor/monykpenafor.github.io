dd2c3bcd7441069b1e73c21d4e3b6007c10fc5c0
Monyk <pal.monik24@gmail.com>
2024-02-28 15:13:22 -0300

    basics - for now playing

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+    const components = document.querySelectorAll('.container .componente');
+  
+    components.forEach(function(component, index) {
+      // Ajuste o fator multiplicativo (2 neste caso) conforme necessário para a amplitude da onda
+      const yOffset = Math.sin((scrollPosition / 200) + (index * 0.5)) * 20;
+      component.style.transform = `translateY(${yOffset}px)`;
+    });
+  });
+  
